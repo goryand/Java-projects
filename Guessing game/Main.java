3c639f7e589d7ddfb71e03108d97089c6344018f
goryand <59327476+goryand@users.noreply.github.com>
2023-05-23 12:49:22 -0400

    Initial commit

--- Guessing game/Main.java
+++ Guessing game/Main.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
 
-// test
+public class Main {
+    public static void main(String[] args) {
+        // Guessing game
+        System.out.println("\n\n");
+        System.out.println("The computer will generate a random number between 1 - 100, and you will have to input numbers to guess it. The computer will tell you if you are either higher, lower, or have guessed the correct number.");
+
+        int triesToGuess = 0;
+        boolean didUserGuessTheNumber = false;
+
+        Scanner scanner = new Scanner(System.in);
+        // Generate the random number that the user will guess
+        int hiddenNumber = (int) Math.round(Math.random() * 100);
+
+        System.out.println("The computer has generated the number. Start guessing now..");
+
+        do {
+            System.out.print("Guess a number: ");
+            short recentlyGuessedVariable = scanner.nextShort();
+            
+            // Add another guess to the counter
+            triesToGuess += 1;
+
+            if (recentlyGuessedVariable == hiddenNumber) {
+                // Player guessed the number. You win!
+                System.out.println("You guessed the correct number! You win!\n It took you " + triesToGuess + " tries.");
+                didUserGuessTheNumber = true;
+            } else if (recentlyGuessedVariable > hiddenNumber) {
+                // Player guessed a number higher than the hidden number.
+                System.out.println("Lower..");
+            } else if (recentlyGuessedVariable < hiddenNumber) {
+                // Player guessed a number lower than the hidden number.
+                System.out.println("Higher..");
+            }
+        } while(didUserGuessTheNumber == false);
+
+        System.out.println("Thank you for playing!");
+        scanner.close();
+    }
+}
--- Pythagorean-theorem-and-quadratic-formula-calculator/Main.java
+++ Pythagorean-theorem-and-quadratic-formula-calculator/Main.java
@@ -1,1 +1,64 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n\n");
+        Scanner scanner = new Scanner(System.in);
+
+        String PythagroeanTheoremInfo = "The Pythagorean theorem, named after the ancient Greek mathematician Pythagoras, dates back to around 500 BCE and is one of the earliest known mathematical principles that continue to be widely used in various fields today.";
+        String QuadraticFormulaInfo = "The quadratic formula, which enables the solution of quadratic equations, has its origins in ancient Babylonian and Greek mathematics. While its precise origin and development are not attributed to a specific individual, it has been a fundamental tool in mathematics for centuries.";
+
+
+        System.out.print("Do you want to use the Pythagorean Theorem (1) or the Quadratic Formula (2)\nAnswer 1 or 2: ");
+        boolean isPythagoreanTheorem;
+
+        // Use ternary operator to write a quick if statement on which equation to do
+        byte numberSent = scanner.nextByte();
+        isPythagoreanTheorem = (numberSent == 1) ? true : false;
+
+        if (isPythagoreanTheorem == true) {
+            // User wants to do the pythagorean theorem
+            System.out.println("You selected the Pythagorean Theorem.");
+            System.out.println(PythagroeanTheoremInfo + "\n");
+
+            // Request all variables
+            System.out.println("Given a triangle has sides a, b, and c,");
+            System.out.print("Input side a: ");
+            float sideA = scanner.nextFloat();
+
+            System.out.print("Input side b: ");
+            float sideB = scanner.nextFloat();
+
+            // Do pythagorean theorem, and return c value
+
+            double sideC = Math.sqrt(((sideA*sideA) + (sideB*sideB)));
+            System.out.println("Side c: " + sideC);
+        } else {
+            // User wants to do the quadratic formula
+            System.out.println("You selected the Quadratic Formula.");
+            System.out.println(QuadraticFormulaInfo + "\n");
+            
+            // Request all variables
+            System.out.println("Given ax^2 + bx + c = 0,");
+            System.out.print("Input variable a: ");
+            float variableA = scanner.nextFloat();
+
+            System.out.print("Input variable b: ");
+            float variableB = scanner.nextFloat();
+
+            System.out.print("Input variable c: ");
+            float variableC = scanner.nextFloat();
+
+            // Do the quadratic formula (two equations because of ±)
+            double answerA = (-variableB + Math.sqrt(variableB * variableB - 4 * variableA * variableC)) / (2 * variableA);
+            double answerB = (-variableB - Math.sqrt(variableB * variableB - 4 * variableA * variableC)) / (2 * variableA);
+
+            // Return both values
+            System.out.println("Roots: " + answerA + ", " + answerB);
+
+        }
+
+        scanner.close();
+    }
+}
